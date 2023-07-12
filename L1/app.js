5fdcd00cfd103d8229bccf6ffbe8f59208477271
Sanjit Kumar <sanjit.kumar@intimetec.com>
2023-07-12 23:11:47 +0530

    added basic of functional component

--- L1/app.js
+++ L1/app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 const heading1 = React.createElement("h2", null, "FrontEndFrameworks");
 const listitem1 = React.createElement("li", null, "React");
 const listitem2 = React.createElement("li", null, "Angular");
@@ -10,5 +13,24 @@ const unorderdList = React.createElement(
   listitem3
 );
 const reactRoot = React.createElement("div", null, heading1, unorderdList);
+const heading = <div>New heading</div>
+const mainHeading = "Welcome to React JS";
+const Header = () => {
+  return (
+    <div id="root">
+      {
+        mainHeading
+      }
+      {
+        console.log("welcome to ReactJS")
+      }
+      <h1> Namaste React</h1>
+    </div>
+  )
+}
+const element = <div>
+  <h1>Hello!</h1>
+  <h2>Good to see you here.</h2>
+</div>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(reactRoot);
+root.render(element);
